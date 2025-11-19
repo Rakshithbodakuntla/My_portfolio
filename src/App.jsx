e72988850dd40ac99426b88bd2e41f3581be6474
Bodakuntla Rakshith <bodakuntlarakshith1@gmail.com>
2025-11-18 23:03:05 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,7 @@ export default function Portfolio() {
   const [formData, setFormData] = useState({ name: "", email: "", message: "" });
 
   // FIX 1: Ensures the scroll position is reset to the top (0, 0) when the component mounts.
-  useEffect(() => {
-    window.scrollTo(0, 0); 
-  }, []);
+
 
   // FIX 2: OBSERVER LOGIC TO UPDATE ACTIVE SECTION IN NAVBAR
   useEffect(() => {
